d2df444ad212dee6e5043544be14ab22c1b1b8d6
Tomonori Suga <t.suga1108@gmail.com>
2020-09-01 21:49:04 +0900

    Can system() (less than 100) in background, or -f.

--- much-once-pacman.sh
+++ much-once-pacman.sh
@@ -10,9 +10,11 @@ debug=:
 n=20
 OPTIND=1
 separately=
+back=
+force=
 RE=${1:-linux}
 test $# -eq 0 || shift
-while getopts n:lds o; do
+while getopts n:ldsSf o; do
 	case "$o" in
 	n) n=$OPTARG;;
 	l) operate='-Q';;
@@ -20,7 +22,9 @@ while getopts n:lds o; do
 echo >&2 x "$@" | sed '1s/^x //'
 EOL`
 	   ;;
+	S) separately=1; back=1;;
 	s) separately=1;;
+	f) force=1;;
 	*) echo "Abort..."; exit 1;;
 	esac
 done ; shift `expr $OPTIND - 1`
@@ -47,8 +51,34 @@ $pkgs
 EOL
 fi`
 
+if test 100 -gt $n || test -n "$force"; then
+	:
+elif test -n "$back"; then
+	cat >&2 <<\EOL
+Your PC may freeze because the numbers of processes which will system'()" is greater than or equal to 100.
+And pid is plus 100, much processes will run in background.
+So abort. That is ignored with -f flag.
+EOL
+		exit 2
+else
+	:
+fi
+
+bp=
+trap '
+	for p in $bp; do
+		kill -0 $p >/dev/null 2>&1 && kill -9 "$p"
+	done
+' 1 2 3 15
+
 start=`/bin/date '+%s'`
-if test -n "$separately"; then
+if test -n "$separately" && test -n "$back"; then
+	for p in $pkgs; do
+		pacman $operate -ii $p &
+		bp="$bp $!"
+	done
+	wait
+elif test -n "$separately" && test -z "$back"; then
 	for p in $pkgs; do
 		pacman $operate -ii $p
 	done
